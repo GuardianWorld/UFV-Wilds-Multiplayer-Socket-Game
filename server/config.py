efdc240da08b7bb550f87eb0804eaa38698ebfec
GuardianWorld <emanuelvitorsam@gmail.com>
2024-07-27 13:35:18 -0300

    start making match system

--- server/config.py
+++ server/config.py
@@ -2,4 +2,6 @@ import threading
 
 active_connections = {}
 logged_users = {}
+searching_for_match = {}
+match_rooms = {}
 stop_event = threading.Event()
--- server/match.py
+++ server/match.py
@@ -6,9 +6,22 @@ import time
 import signal
 import database
 
-from config import active_connections, active_sockets, stop_event
+from config import active_connections, logged_users, stop_event, searching_for_match, match_rooms
 
-def search_match():
-    
-    print("haai")
+def search_match(player_socket, player_name):
+    searching_for_match[player_name] = player_socket
+    player_socket.send(json.dumps({"status": 200, "message": "Searching for match...", "command": "searching"}).encode())
+    while not stop_event.is_set():
+        for second_player in searching_for_match:
+            if second_player != player_name:
+                match_rooms[len(match_rooms)] = {player_name, second_player}
+                del searching_for_match[player_name]
+                del searching_for_match[second_player]
+                player_socket.send(json.dumps({"status": 200, "message": "Match found!", "command": "match"}).encode())
+                searching_for_match[second_player].send(json.dumps({"status": 200, "message": "Match found!", "command": "match"}).encode())
+        if player_name in match_rooms:
+            player_socket.send(json.dumps({"status": 200, "message": "Match found!", "command": "match"}).encode())
+            break
+        time.sleep(1)
+
     
